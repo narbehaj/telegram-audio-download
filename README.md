c3c275d640f3b4b070a18c685ab05c138dedc382
Narbeh <narbeh@protonmail.com>
2018-01-20 17:41:02 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# **Telegram Audio Download**
+
+This script uses Telepot python framework to connect to Telegram's API in order to control your bot. After creating your bot via @BotFather, paste your bot's token in the script then run it. You can simply forward as many as audio files (Max 100 files by Telegram)  to your bot and it'll download the forwarded files. It will simply skip the files which have been downloaded already.
+
+### Requirements
+
+You only need `telepot` for this.
+
+`pip3 install telepot`
+
+### Usage
+
+`python3 tg_audio.py`
+
+### Screenshots
+
+<img src="screenshots/1.png" style="zoom:30%" />
+
+<img src="screenshots/2.png" style="zoom:30%" />
+
+<img src="screenshots/3.png" style="zoom:30%" />
+
+<img src="screenshots/4.png" style="zoom:70%" />
+
+<img src="screenshots/5.png" style="zoom:70%" />
---
+++ tg_audio.py
@@ -0,0 +1,89 @@
+import telepot
+
+from requests import get
+from random import randint
+from time import sleep
+from json import loads
+from os import path, mkdir
+
+
+# Your registered bot's token
+bot_token = ''
+
+
+def get_file_path(token, file_id):
+    get_path = get('https://api.telegram.org/bot{}/getFile?file_id={}'.format(token, file_id))
+    json_doc = loads(get_path.text)
+    try:
+        file_path = json_doc['result']['file_path']
+    except Exception as e:  # Happens when the file size is bigger than the API condition
+        print(e)
+        bot.sendMessage(chat_id, 'Failed for {}'.format(singer, song_name))
+        return None
+
+    return 'https://api.telegram.org/file/bot{}/{}'.format(token, file_path)
+
+
+def get_file(msg_list, chat_id):
+    if len(msg_list) > 1:
+        msg_count = len(msg_list)
+        print('Total files: {}'.format(msg_count))
+
+    for msg in msg_list:
+        try:
+            mp3id = msg['message']['audio']['file_id']
+        except KeyError:
+            continue
+
+        try:
+            singer = msg['message']['audio']['performer']
+        except:
+            singer = 'Unknown'
+
+        # Remove / and - characters to create directory
+        singer_dir = singer.replace('/', '-').strip()
+
+        try:
+            song_name = msg['message']['audio']['title']
+        except:
+            song_name = str(randint(120, 1900000000))
+
+        if path.exists('{}/{}_{}.mp3'.format(singer_dir, singer, song_name)):
+            print('{} {} --> File exists'.format(singer, song_name))
+            continue
+
+        print('Working on --> {} {}'.format(singer, song_name))
+        # Get file download path
+        download_url = get_file_path(bot_token, mp3id)
+        mp3file = get(download_url)
+
+        if not path.exists(singer_dir):
+            mkdir(singer_dir)
+
+        try:
+            with open('{}/{}_{}.mp3'.format(singer_dir, singer, song_name), 'wb') as f:
+                f.write(mp3file.content)
+        except FileNotFoundError:
+            with open('{}.mp3'.format(randint(120, 1900000000)), 'wb') as f:
+                f.write(mp3file.content)
+
+        bot.sendMessage(chat_id, 'Done: {} {}'.format(singer, song_name))
+
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    usermsg = bot.getUpdates(allowed_updates='message')
+    get_file(usermsg, chat_id)
+
+
+def main():
+    bot.message_loop(handle)
+    # Keep the program running
+    while 1:
+        sleep(10)
+
+
+if __name__ == '__main__':
+    bot = telepot.Bot(bot_token)
+    main()
+
